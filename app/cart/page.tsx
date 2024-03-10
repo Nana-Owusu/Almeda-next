4e32d6acb02e059aa17e9ae8d7ce901780668a60
Nana-Owusu <owusuagyei8@gmail.com>
2024-03-10 13:35:55 +0000

    cart

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -5,26 +5,29 @@ const Cart = () => {
     <>
       <section className="bg-[#cccccc21]">
         <div className="container mx-auto px-4">
-          <div className="p-14">
+          <div className=" lg:px-32  lg:py-16">
             <div className="space-y-3">
               <h2 className="text-xl">Shopping Cart</h2>
               <div>
-                <div className="grid grid-cols-3 pt-10 border-b-2">
-                  <div className=" flex space-x-2">
+                <div className="grid md:grid-cols-4 pt-10 pb-5 border-b-2">
+                  <div className=" flex space-x-2 col-span-2">
                     <div>
                       <img
                         src="/images/kimem-jacky-bicolor-waist-trousers-navy-black_0374-v1-FINAL-copy.jpg"
                         alt="clothing"
-                        className="w-[180px] h-[200px]"
+                        className=" w-[90px] md:w-[180px] md:h-[150px]"
                       />
                     </div>
                     <div className="space-y-2">
-                      <h2 className="captalize text-xl">
+                      <h2 className="captalize md:text-base lg:text-lg">
                         Lounge Tunic / Black
                       </h2>
-                      <p className="font-light text-[#676666]">Size 2</p>
+                      <p className="font-light text-[#676666] text-sm lg:text-base">
+                        Size 2
+                      </p>
                     </div>
                   </div>
+                  {/* <div></div> */}
                   <div className="flex space-x-1 justify-end">
                     <div>
                       <img
@@ -32,21 +35,22 @@ const Cart = () => {
                         alt="remove"
                       />
                     </div>
-                    <div className="text-lg">
+                    <div className=" md:text-base lg:text-lg">
                       <input
                         type="text"
+                        value={1}
                         className="w-[35px] text-center bg-[#cccccc00]"
                       />
                     </div>
                     <div>
                       <img
-                        src="/close_FILL0_wght400_GRAD0_opsz24.svg"
+                        src="/add_FILL0_wght400_GRAD0_opsz24.svg"
                         alt="add"
                       />
                     </div>
                   </div>
                   <div className="flex space-x-4 justify-end">
-                    <div>$ 150.00</div>
+                    <div className=" md:text-base lg:text-lg">$ 150.00</div>
                     <div>
                       <img
                         src="/close_FILL0_wght400_GRAD0_opsz24.svg"
@@ -56,8 +60,8 @@ const Cart = () => {
                   </div>
                 </div>
               </div>
-              <div className="grid justify-end ">
-                <div className="flex space-x-72 mb-7 mt-5 items-center">
+              <div className="grid lg:justify-end ">
+                <div className="flex justify-between lg:space-x-72 mb-7 mt-5 items-center">
                   <div>Sub Total</div>
                   <div className="text-2xl">$800.00</div>
                 </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
+import CategoriesNav from "@/components/CategoriesNav";
 import Heronav from "@/components/heronav";
 import Products from "@/components/products-shop/products";
 
 export default function Home() {
   return (
     <>
-      <Product />
+      <div className="mt-28">
+        <CategoriesNav />
+      </div>
+      <Products />
     </>
   );
 }
